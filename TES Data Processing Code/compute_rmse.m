0ce178aaf1357d742c2363863e13dda67422505f
nadjamarin <124326162+nadjamarin@users.noreply.github.com>
2023-04-09 14:32:23 -0500

    Add header

--- TES Data Processing Code/compute_rmse.m
+++ TES Data Processing Code/compute_rmse.m
@@ -1,4 +1,9 @@
 function rmse = compute_rmse(estimate, ref)
+% This file is used for processing data from the Torso-dynamics Estimation System (TES) Validation Study.
+    % Link to corresponding paper: https://doi.org/10.36227/techrxiv.22336843.v1
+% All code files (including this one) can be found here: https://github.com/ssong47/TorsodynamicsEstimationSystem
+% The data can be found here: https://ieee-dataport.org/documents/validation-study-torso-dynamics-estimation-system-tes-hands-free-physical-human-robot
+
 % computes RMSE given reference and estimate
 
 % find number of data points in the reference and estimate
