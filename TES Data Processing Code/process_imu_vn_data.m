5a880abb9e47c57c85484ceb42af9d3825b488a7
nadjamarin <124326162+nadjamarin@users.noreply.github.com>
2023-04-09 14:34:06 -0500

    Add header

--- TES Data Processing Code/process_imu_vn_data.m
+++ TES Data Processing Code/process_imu_vn_data.m
@@ -1,4 +1,9 @@
 function vn_angles = process_imu_vn_data(raw_vn_angles)
+% This file is used for processing data from the Torso-dynamics Estimation System (TES) Validation Study.
+    % Link to corresponding paper: https://doi.org/10.36227/techrxiv.22336843.v1
+% All code files (including this one) can be found here: https://github.com/ssong47/TorsodynamicsEstimationSystem
+% The data can be found here: https://ieee-dataport.org/documents/validation-study-torso-dynamics-estimation-system-tes-hands-free-physical-human-robot
+
 % Computes torso angles (yaw, pitch, roll) from VN IMU data
 
 vn_angles = raw_vn_angles;
