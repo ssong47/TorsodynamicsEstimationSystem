a940da3ea723a827d947004e7fd4d74f3a435821
nadjamarin <124326162+nadjamarin@users.noreply.github.com>
2023-04-09 14:30:27 -0500

    Add header

--- TES Data Processing Code/compute_COP.m
+++ TES Data Processing Code/compute_COP.m
@@ -1,4 +1,9 @@
 function [fss_COPx, fss_COPy] = compute_COP(raw_force_data)
+% This file is used for processing data from the Torso-dynamics Estimation System (TES) Validation Study.
+    % Link to corresponding paper: https://doi.org/10.36227/techrxiv.22336843.v1
+% All code files (including this one) can be found here: https://github.com/ssong47/TorsodynamicsEstimationSystem
+% The data can be found here: https://ieee-dataport.org/documents/validation-study-torso-dynamics-estimation-system-tes-hands-free-physical-human-robot
+
 % Compute the center of pressure (COP) components from the force data
 % Force data can be from AMTI or FSS
 
