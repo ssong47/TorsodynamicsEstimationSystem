502fef6eb7d9840bf7b2dda8aca32dd995370bcd
nadjamarin <124326162+nadjamarin@users.noreply.github.com>
2023-04-09 14:31:16 -0500

    Add header

--- TES Data Processing Code/compute_angle_two_v.m
+++ TES Data Processing Code/compute_angle_two_v.m
@@ -1,4 +1,9 @@
 function angle = compute_angle_two_v(u, v)
+% This file is used for processing data from the Torso-dynamics Estimation System (TES) Validation Study.
+    % Link to corresponding paper: https://doi.org/10.36227/techrxiv.22336843.v1
+% All code files (including this one) can be found here: https://github.com/ssong47/TorsodynamicsEstimationSystem
+% The data can be found here: https://ieee-dataport.org/documents/validation-study-torso-dynamics-estimation-system-tes-hands-free-physical-human-robot
+
 % Computes the angle between two vectors u and v in 3D space (x, y, z)
 
 % solve for cos(theta) using formula for angle between two 3D vectors
